cc311f8d1247d51a88554ad6fd122c98191fde28
Kay <80385994+twilightyear@users.noreply.github.com>
2026-08-29 13:50:21 +0900

    solv: [LeetCode] 739. Daily Temperatures ( Stack )

---
+++ Leet-Code/739-daily-temperatures/739-daily-temperatures-2-(stack).py
@@ -0,0 +1,14 @@
+#풀이 2 (Stack)
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        result = [0]*n
+        stack = []
+
+        for i, temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
+                prev_index = stack.pop()
+                result[prev_index] = i - prev_index
+            stack.append(i)
+
+        return result
