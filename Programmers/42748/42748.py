7ab770c942416564625ad86f54f4ec7bf67fb9eb
Kay <80385994+twilightyear@users.noreply.github.com>
2026-09-26 12:26:40 +0900

    solv: [Programers] K 번째 수

---
+++ Programmers/42748/42748.py
@@ -0,0 +1,6 @@
+# 풀이 1 (Sort)
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        answer.append(sorted(array[command[0]-1:command[1]])[command[2]-1])
+    return answer
